f1694a6104bc369977c62bcad14db68b17ab91cf
ProGamerGov <ProGamerGov@users.noreply.github.com>
2025-02-07 10:10:22 -0700

    Update pytorch360convert.py

--- pytorch360convert/pytorch360convert.py
+++ pytorch360convert/pytorch360convert.py
@@ -842,7 +842,7 @@ def e2c(
         e_img (torch.Tensor): Input equirectangular image tensor of shape
             [C, H, W] or [H, W, C].
         face_w (int, optional): Width of each square cube shaped face. If set to None,
-		    then face_w will be calculated as H // 2. Default: 256
+                    then face_w will be calculated as H // 2. Default: 256
         mode (str, optional): Sampling interpolation mode, 'nearest',
             'bicubic', or 'bilinear'. Default: 'bilinear'.
         cube_format (str, optional): The desired output cubemap format. Options
